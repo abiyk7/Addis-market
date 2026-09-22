f3dba90d36de03310491ce8b07930d532295281d
abiyk7 <ab.kassa7@gmail.com>
2026-09-22 05:06:20 +0200

    Add sitemap function to generate site URLs

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  const base = "https://addis-market.com";
+  return [
+    { url: base, lastModified: new Date() },
+    { url: `${base}/about`, lastModified: new Date() },
+    { url: `${base}/contact`, lastModified: new Date() },
+    { url: `${base}/privacy`, lastModified: new Date() },
+    { url: `${base}/terms`, lastModified: new Date() },
+  ];
+}
